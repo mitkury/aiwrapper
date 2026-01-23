51693d2147d6d58aae8b080e241f8a0d7f7345cc
Dmitry Kury <dima.kurilchenko@gmail.com>
2026-01-23 08:53:48 +0000

    tests: remove "old-tests" exlusion

--- vitest.config.js
+++ vitest.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['tests/**/*.test.{js,ts}'],
-    exclude: ['tests-old/**/*'],
     globals: true,
     testTimeout: 1000 * 60 * 5, // 5 minutes timeout
     setupFiles: ['./tests/setup.js'] // Setup file to load environment variables
